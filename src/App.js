e18f1d5893d303a297bb7f3585d5dca56a1d6ff3
John Rae <john.rae23@gmail.com>
2021-09-07 18:38:21 -0400

    Build pls

--- src/App.js
+++ src/App.js
@@ -155,7 +155,7 @@ function InstancedSpheres({ count = 200, selectedPallete }) {
         .convertSRGBToLinear()
         .toArray(array, i * 3);
     return array;
-  }, [count]);
+  }, [count, colorsArray]);
 
   return (
     <instancedMesh
